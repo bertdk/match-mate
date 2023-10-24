a7cff01cf740228a7f8cb32fa1e8f7bf6b3c5868
bertdk <bertdk10@gmail.com>
2023-10-24 17:43:53 +0200

    fix: remove exclude and expose

--- apps/match/src/app/game/contracts/score.view.ts
+++ apps/match/src/app/game/contracts/score.view.ts
@@ -1,19 +1,14 @@
 import { Nested } from '@match-mate-api/core-utils';
-import { Exclude, Expose } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
 import { PlayerView } from '../../player/contracts';
 
-@Exclude()
 export class ScoreView {
-  @Expose()
   @IsString()
   public id: string;
 
-  @Expose()
   @IsNumber()
   public gamePoints: number;
 
-  @Expose()
   @Nested()
   public player: PlayerView;
 }
--- apps/match/src/app/tournament/contracts/tournamentRanking.view.ts
+++ apps/match/src/app/tournament/contracts/tournamentRanking.view.ts
@@ -1,18 +1,13 @@
 import { makeListView } from '@match-mate-api/nest-utils';
-import { Exclude, Expose } from 'class-transformer';
 import { IsNumber, IsString, IsUUID } from 'class-validator';
 
-@Exclude()
 export class TournamentRankingView {
-  @Expose()
   @IsUUID()
   public id: string;
 
-  @Expose()
   @IsString()
   public name: string;
 
-  @Expose()
   @IsNumber()
   public points: number;
 }
