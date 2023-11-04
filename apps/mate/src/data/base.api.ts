c0f457c20eb272988ac6267e6397f9bd4c93b1af
bertdk <bertdk10@gmail.com>
2023-11-04 14:27:07 +0100

    fix: no nested env var

--- apps/mate/src/data/base.api.ts
+++ apps/mate/src/data/base.api.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 import config from 'src/config';
 
-export const api = axios.create({ baseURL: config.api.url });
+export const api = axios.create({ baseURL: config.apiUrl });
